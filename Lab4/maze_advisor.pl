0343c0bc2e58449c8f3a3332d75594e65e8074fd
Erick Olivo <OliauErick@outlook.com>
2025-10-24 18:43:53 -0500

    Implement reasoning rules (can_move/2, reason/3)

--- Lab4/maze_advisor.pl
+++ Lab4/maze_advisor.pl
@@ -13,3 +13,23 @@ edge(c, b).
 
 blocked(a, c). % Door is blocked from a to c
 
+
+% PART 2 - Reasoning Rules
+
+% We can move from X to Y if an 'edge' from X to Y exists AND (,) it is NOT (\+) blocked.
+
+can_move(X, Y) :-
+    edge(X, Y),
+    \+ blocked(X, Y).
+
+
+reason(X, Y, 'path is open') :-
+    can_move(X, Y).
+
+reason(X, Y, 'path is blocked') :-
+    blocked(X, Y).
+
+reason(_X, Y, 'destination reached') :-
+    Y == exit.
+
+
