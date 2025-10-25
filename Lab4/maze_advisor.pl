f1904bb83c8ab2a409a8f3db33ab03155dfe30c6
Erick Olivo <OliauErick@outlook.com>
2025-10-24 19:27:05 -0500

    Implement recursive move/4 predicate

--- Lab4/maze_advisor.pl
+++ Lab4/maze_advisor.pl
@@ -22,14 +22,34 @@ can_move(X, Y) :-
     edge(X, Y),
     \+ blocked(X, Y).
 
-
+% reason/3 rules
 reason(X, Y, 'path is open') :-
     can_move(X, Y).
 
 reason(X, Y, 'path is blocked') :-
     blocked(X, Y).
 
+% Extending reason/3
 reason(_X, Y, 'destination reached') :-
     Y == exit.
 
+
+% PART 3 - Recursive Traversal
+
+
+% move(Current, Destination, Visited, FinalPath)
+
+% Base Case: We have arrived. If we can move from X (current) to Y (destination).
+
+move(X, Y, Visited, [Y|Visited]) :-
+    can_move(X, Y),
+    format('Moving from ~w to ~w.~n', [X, Y]).
+
+% Recursive Case: We take a step and keep searching. To get from X to Y (destination)
+
+move(X, Y, Visited, Path) :-
+    can_move(X, Z),                                     % 1. We can move to an intermediate node Z
+    \+ member(Z, Visited),                              % 2. And we have not visited Z before (prevents loops)
+    format('Exploring from ~w to ~w...~n', [X, Z]),     % 3. Print the exploration
+    move(Z, Y, [Z|Visited], Path).                      % 4. Recursively search from Z to Y
 
